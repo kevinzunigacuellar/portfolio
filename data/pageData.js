6921ad9a1cbb83bb876b8091c20344c1f3380009
Kevin Z - Razer <kzunigac@uvm.edu>
2021-09-16 19:48:46 -0400

    added static data for bookshelf page

---
+++ data/pageData.js
@@ -0,0 +1,28 @@
+export const homeData = {
+  en: {
+    welcome: 'Hello, I am Kevin Zuniga',
+    description:
+      'Welcome to my slice of the internet. I am a graduate student in mechanical engineering at University of Vermont. In my free time I enjoy making websites and experimenting with web app technologies.',
+    postsTitle: 'Latest posts:',
+  },
+  es: {
+    welcome: 'Hola, soy Kevin Zuñiga',
+    description:
+      'Bienvenido a mi rebanada en la internet. Soy un estudiante de posgrado en ingeniería mecánica en la Universidad de Vermont. En mi tiempo libre me dedico a hacer paginas web y experimentar con nuevas tecnologías.',
+    postsTitle: 'Últimas publicaciones:',
+  },
+}
+
+export const bookShelfData = {
+  en: {
+    pageTitle: 'My book shelf',
+    pageDescription:
+      'I love reading so I thought of making a book shelf were I share all the books I finished cover to cover',
+  },
+
+  es: {
+    pageTitle: 'Mi estante de libros',
+    pageDescription:
+      'Me gusta leer, por eso pense en hacer un estante de libros donde compartire todos los libros que he terminado de tapa a tapa',
+  },
+}
