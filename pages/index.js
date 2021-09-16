c7c5f6380df6df1482b507df32519dc05829e8cf
Kevin Z - Razer <kzunigac@uvm.edu>
2021-09-16 09:59:35 -0400

    removed top margin from description in articles card

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home({ blogPostsData, homeDataLocale }) {
                     {title}
                   </h2>
                   <p className='text-gray-500 dark:text-gray-400'>{date}</p>
-                  <p className='mt-1 text-gray-500 dark:text-gray-400'>
+                  <p className='text-gray-500 dark:text-gray-400'>
                     {description}
                   </p>
                 </a>
