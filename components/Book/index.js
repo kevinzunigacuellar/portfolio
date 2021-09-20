ec8a16db74625a5bec19b9b10347e658296fb702
Kevin Z - Razer <kzunigac@uvm.edu>
2021-09-20 14:57:07 -0400

    added more books to bookshelf

--- components/Book/index.js
+++ components/Book/index.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 export default function Book({ img, title, author, year, comment }) {
   return (
-    <div className='mx-auto max-w-md md:max-w-2xl bg-white rounded-lg border overflow-hidden'>
+    <div className='max-w-md md:max-w-2xl bg-white rounded-lg border overflow-hidden dark:bg-gray-900 dark:border-gray-500 transition-colors'>
       <div className='md:flex'>
         <div className='max-w-md'>
           <div className='relative pb-3/2 md:w-60'>
@@ -14,13 +14,17 @@ export default function Book({ img, title, author, year, comment }) {
           </div>
         </div>
         <div className='py-4 px-6'>
-          <p className='font-semibold py-1 text-lg'>{title}</p>
-          <p>
-            {author}
-            <span className='text-gray-500'>&middot;</span>{' '}
-            <span className='text-gray-500'>{year}</span>
+          <p className='font-semibold py-1 text-lg dark:text-gray-200'>
+            {title}
           </p>
-          <q className='text-gray-500 leading-relaxed'>{comment}</q>
+          <p>
+            <span className='text-gray-800 dark:text-gray-300'>{author}</span>{' '}
+            <span className='text-gray-500 dark:text-gray-400'>&middot;</span>{' '}
+            <span className='text-gray-500 dark:text-gray-400'>{year}</span>
+          </p>
+          <q className='text-gray-500 leading-relaxed dark:text-gray-400'>
+            {comment}
+          </q>
         </div>
       </div>
     </div>
--- data/pageData.js
+++ data/pageData.js
@@ -26,7 +26,32 @@ export const bookShelfData = {
         title: 'Eloquent JavaScript',
         author: 'Marijn Haverbeke',
         year: '2018',
-        comment: 'Hands down one of the best JavaScript books.',
+        comment:
+          'I started my JavaScript journey with this book and I still go back to it from time to time',
+      },
+      {
+        img: '/img/sam_walton.jpg',
+        title: 'Sam Walton: Made In America',
+        author: 'Sam Walton',
+        year: '1993',
+        comment:
+          'First book I have ever finished cover to cover. More than Sam Walton biography, is the story of how Walmart started and how it became the store we know today',
+      },
+      {
+        img: '/img/the_cow.jpg',
+        title: 'The Cow',
+        author: 'Dr. Camilo Cruz',
+        year: '2004',
+        comment:
+          'I picked up this book from my parents bookshelf. I failed to see get the moral of the story the first time I read it. A few years later I decided to read it again. One of the best motivation books I have ever read.',
+      },
+      {
+        img: '/img/aprendiendo_git.png',
+        title: 'Aprendiendo Git',
+        author: 'Miguel Angel Durán',
+        year: '2021',
+        comment:
+          'I bought this book on presale to support one of my favorites twitch streamers. I highly recommend it regardless of your knowledge of git.',
       },
     ],
   },
@@ -42,6 +67,30 @@ export const bookShelfData = {
         author: 'Marijn Haverbeke',
         year: '2018',
         comment: 'Uno de los mejores libros de JavaScript que he leido.',
+      },
+      {
+        img: '/img/sam_walton.jpg',
+        title: 'Sam Walton: Made In America',
+        author: 'Sam Walton',
+        year: '1993',
+        comment:
+          'Primer libro que termine por completo. Más que la biografía de Sam Walton, es la historia de como empezó Walmart y como con mucha dedicación del fundador logro ser la tienda que conocemos hoy.',
+      },
+      {
+        img: '/img/the_cow.jpg',
+        title: 'La Vaca',
+        author: 'Dr. Camilo Cruz',
+        year: '2004',
+        comment:
+          'Elegí este libro de la biblioteca de mis padres. No lo comprendí la primera vez que lo termine pero después de un tiempo volví a leerlo y sin duda es un gran libro de motivación.',
+      },
+      {
+        img: '/img/aprendiendo_git.png',
+        title: 'Aprendiendo Git',
+        author: 'Miguel Angel Durán',
+        year: '2021',
+        comment:
+          'Compre este libro en la preventa para apoyar a uno de mis streamers favoritos. La verdad que lo recomiendo mucho, está muy fácil de seguir y entender sin importar el nivel de git que conozcas.',
       },
     ],
   },
