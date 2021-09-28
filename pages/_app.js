a1dc125af6a692912042f60314c40ba0f338014d
Kevin Z - Razer <kzunigac@uvm.edu>
2021-09-28 11:01:15 -0400

    added a logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import 'tailwindcss/tailwind.css'
 import Layout from 'components/Layout'
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <link rel='icon' href='/logo.svg' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta charSet='utf-8' />
+        <meta name='keywords' content='software, blog, developer, student' />
+        <link rel='canonical' href='https://www.kevinzunigacuellar.com' />
+        <meta name='author' content='Kevin Zuniga Cuellar' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
