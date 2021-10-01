4bcb917b337791370b4a4bc2e1536fdcdc0df2d7
Kevin Z - Razer <kzunigac@uvm.edu>
2021-10-01 17:37:22 -0400

    fixed footer responsiveness

--- components/Layout/Footer/index.js
+++ components/Layout/Footer/index.js
@@ -10,34 +10,36 @@ export default function Footer() {
   return (
     <footer className='bg-gray-200 p-4 flex-shrink-0 border-t border-gray-300 dark:bg-gray-900 dark:border-gray-500 transition-all'>
       <div className='mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-4xl'>
-        <div className='flex justify-around'>
-          <div className='flex flex-col space-y-5'>
-            <SpotifyPlayer />
-            <div className='flex space-x-5 text-gray-400 dark:text-gray-400'>
-              <a
-                href='https://github.com/kevinzunigacuellar'
-                target='_blank'
-                aria-label='Visit my Github'
-                rel='noreferrer'>
-                <Github />
-              </a>
-              <a
-                href='https://www.linkedin.com/in/kevinzunigacuellar/'
-                target='_blank'
-                aria-label='Visit my LinkedIn'
-                rel='noreferrer'>
-                <LinkedIn />
-              </a>
-            </div>
-          </div>
-          <div className='flex flex-col p-2'>
-            {navigation.map(({ name, href }) => (
-              <Link href={href} key={name + href}>
-                <a className='p-2 cursor-pointer antialiased text-md text-gray-500 hover:text-gray-800 font-semibold dark:text-gray-400 dark:hover:text-gray-300'>
-                  {locale === 'en' ? name.en : name.es}
+        <div className='mx-auto w-72 sm:m-0 sm:w-auto'>
+          <div className='flex flex-col sm:flex-row sm:justify-around'>
+            <div className='flex flex-col space-y-5 sm:w-auto'>
+              <SpotifyPlayer />
+              <div className='flex space-x-5 text-gray-400 dark:text-gray-400'>
+                <a
+                  href='https://github.com/kevinzunigacuellar'
+                  target='_blank'
+                  aria-label='Visit my Github'
+                  rel='noreferrer'>
+                  <Github />
                 </a>
-              </Link>
-            ))}
+                <a
+                  href='https://www.linkedin.com/in/kevinzunigacuellar/'
+                  target='_blank'
+                  aria-label='Visit my LinkedIn'
+                  rel='noreferrer'>
+                  <LinkedIn />
+                </a>
+              </div>
+            </div>
+            <div className='flex flex-col py-2 sm:w-auto sm:max-w-md'>
+              {navigation.map(({ name, href }) => (
+                <Link href={href} key={name + href}>
+                  <a className='py-2 cursor-pointer antialiased text-md text-gray-500 hover:text-gray-800 font-semibold dark:text-gray-400 dark:hover:text-gray-300'>
+                    {locale === 'en' ? name.en : name.es}
+                  </a>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </div>
