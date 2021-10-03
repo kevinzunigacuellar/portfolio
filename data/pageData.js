316ce582dc65db2957618d540a6c1948e3b7068f
Kevin Z - Razer <kzunigac@uvm.edu>
2021-10-03 11:34:25 -0400

    fixed index title translation

--- data/pageData.js
+++ data/pageData.js
@@ -1,7 +1,7 @@
 export const homeData = {
   en: {
     pageTitle:
-      'Kevin Zuniga Cuellar - Ingeniero mecánico y desarrollador web autodidacta',
+      'Kevin Zuniga Cuellar - Mechanical Engineer and self-taught software developer',
     welcome: 'Hello, I am Kevin Zuniga',
     description:
       'Welcome to my slice of the internet. I am a graduate student in mechanical engineering at University of Vermont. In my free time I enjoy making websites and experimenting with web app technologies.',
@@ -9,7 +9,7 @@ export const homeData = {
   },
   es: {
     pageTitle:
-      'Kevin Zuniga Cuellar - Mechanical Engineer and self-taught software developer',
+      'Kevin Zuniga Cuellar - Ingeniero mecánico y desarrollador web autodidacta',
     welcome: 'Hola, soy Kevin Zuñiga',
     description:
       'Bienvenido a mi rebanada en la internet. Soy un estudiante de posgrado en ingeniería mecánica en la Universidad de Vermont. En mi tiempo libre me dedico a hacer paginas web y experimentar con nuevas tecnologías.',
