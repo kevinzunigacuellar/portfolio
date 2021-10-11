d77a1fff5e80aefac2fdc7088a1a776e29c23388
Kevin Z - Razer <kzunigac@uvm.edu>
2021-10-11 11:50:55 -0400

    added a spotify player placeholder

--- components/SpotifyPlayer/index.js
+++ components/SpotifyPlayer/index.js
@@ -2,13 +2,14 @@ import NotPlaying from '/components/SpotifyPlayer/NotPlaying'
 import CurrentlyPlaying from '/components/SpotifyPlayer/CurrentlyPlaying'
 import { getSong } from '/services/getSong'
 import useSWR from 'swr'
+import SpotifyPlaceholder from './SpotifyPlaceholder'
 
 export default function SpotifyPlayer() {
   const { data, error } = useSWR('/api/spotify_current_song', getSong)
 
   if (error) return <NotPlaying />
 
-  if (!data) return <pre>getting song...</pre>
+  if (!data) return <SpotifyPlaceholder />
 
   if (data && data.isPlaying === false) return <NotPlaying />
 
