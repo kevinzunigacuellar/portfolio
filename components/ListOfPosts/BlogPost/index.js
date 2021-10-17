1547e96748e91ec43d2928f08a939d711e82832f
Kevin Z - Razer <kzunigac@uvm.edu>
2021-10-16 20:23:45 -0400

    change post title size

--- components/ListOfPosts/BlogPost/index.js
+++ components/ListOfPosts/BlogPost/index.js
@@ -1,7 +1,7 @@
 export default function BlogPost({ title, date, description }) {
   return (
     <div className='max-w-4xl shadow rounded py-4 px-6 border-l-4 border-indigo-400 hover:border-indigo-500 dark:hover:border-indigo-600 hover:shadow-md dark:bg-gray-900 bg-white transition-all'>
-      <h2 className='pb-2 text-gray-900 font-semibold dark:text-gray-200'>
+      <h2 className='text-lg pb-2 text-gray-900 font-semibold dark:text-gray-200'>
         {title}
       </h2>
       <p className='text-gray-500 leading-relaxed dark:text-gray-400'>{date}</p>
