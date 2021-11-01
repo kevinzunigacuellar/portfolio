c8f6b4f585a3f663edf15cc9bbf1d1f5a12abcc5
Kevin Z - Razer <kzunigac@uvm.edu>
2021-10-31 20:51:00 -0400

    deleted unused components

--- components/Subtitle/index.js
+++
@@ -1,7 +0,0 @@
-export default function Subtitle({ children }) {
-  return (
-    <h2 className='text-xl sm:text-2xl font-bold text-gray-900 dark:text-gray-100 leading-relaxed'>
-      {children}
-    </h2>
-  )
-}
--- components/Title/index.js
+++
@@ -1,7 +0,0 @@
-export default function Title({ children }) {
-  return (
-    <h1 className='text-2xl sm:text-3xl font-bold text-gray-900 dark:text-gray-100'>
-      {children}
-    </h1>
-  )
-}
