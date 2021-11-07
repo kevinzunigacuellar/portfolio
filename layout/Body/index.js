8413d54e8a0dcc7d979b27ea8040fafe79d5eba9
Kevin Z - Razer <kzunigac@uvm.edu>
2021-11-07 13:03:25 -0500

    removed body background color

--- layout/Body/index.js
+++ layout/Body/index.js
@@ -1,6 +1,6 @@
 export default function Body({ children }) {
   return (
-    <main className='bg-gray-100 dark:bg-gray-800 transition-colors flex-grow'>
+    <main className='flex-grow'>
       <div className='mx-auto px-6 w-full sm:max-w-2xl md:max-w-3xl lg:max-w-4xl'>
         {children}
       </div>
