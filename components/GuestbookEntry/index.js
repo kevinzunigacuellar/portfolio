cead5ea4669b4521cead5cac02cc7db018b8aeee
Kevin Z - Razer <kzunigac@uvm.edu>
2021-12-01 12:01:53 -0500

    created an entry component

---
+++ components/GuestbookEntry/index.js
@@ -0,0 +1,23 @@
+import DateFormater from 'components/DateFormater'
+
+export default function GuestbookEntry({ author, datetime, message }) {
+  return (
+    <article className='space-y-3 pb-6'>
+      <p>{message}</p>
+      <div className='flex space-x-1 items-center text-sm text-gray-600 dark:text-gray-400'>
+        <p>{author} &middot;</p>
+        <DateFormater
+          date={datetime}
+          className=''
+          format={{
+            year: '2-digit',
+            month: 'short',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+          }}
+        />
+      </div>
+    </article>
+  )
+}
