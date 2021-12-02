54edd7bbb484d3550b87e155e3b28b80cadd514c
Kevin Z - Razer <kzunigac@uvm.edu>
2021-12-02 18:40:56 -0500

    fixed create_entry api

--- pages/api/createEntry.js
+++ pages/api/createEntry.js
@@ -1,4 +1,5 @@
 import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0'
+import prisma from 'lib/prisma'
 
 export default withApiAuthRequired(async function handler(req, res) {
   if (req.method !== 'POST') {
@@ -9,10 +10,10 @@ export default withApiAuthRequired(async function handler(req, res) {
     const { name } = session.user
     const { message } = req.body
     const guestbook = await prisma.guestbook.create({
-      data: { author: name, message },
+      data: { author: name || 'Guest', message },
     })
     res.status(200).json(guestbook)
   } catch (error) {
-    res.status(500).json({ error })
+    res.status(500).json({ msg: error.message })
   }
 })
