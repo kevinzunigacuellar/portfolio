418861fe72ab0665eba16e4caf38125d5ec30772
Kevin Z - Razer <kzunigac@uvm.edu>
2021-12-03 18:22:35 -0500

    added react-hot-toast

--- components/Forms/SignEntryForm.js
+++ components/Forms/SignEntryForm.js
@@ -2,11 +2,27 @@ import { Formik, Form } from 'formik'
 import { TextInput } from 'components/Forms/Fields'
 import { mutate } from 'swr'
 import axios from 'axios'
+import toast, { Toaster } from 'react-hot-toast'
 
 export default function SignEntry() {
   const handleCreateEntry = async values => {
-    await axios.post('/api/createEntry', values)
-    await mutate('/api/guestbook')
+    toast.promise(
+      axios.post('/api/createEntry', values),
+      {
+        loading: 'Posting...',
+        success: 'Thank you for your comment',
+        error: 'Something went wrong',
+      },
+      {
+        style: {
+          minWidth: '200px',
+        },
+        success: {
+          duration: 5000,
+        },
+      }
+    )
+    mutate('/api/guestbook')
   }
 
   return (
@@ -29,6 +45,7 @@ export default function SignEntry() {
           className='block px-10 hover:border-indigo-400 hover:bg-indigo-200 dark:text-gray-100 bg-indigo-100 transition-colors border border-indigo-300 dark:border-gray-500 rounded-lg py-2 font-semibold text-indigo-700 dark:bg-gray-900 dark:hover:border-gray-300'>
           Sign
         </button>
+        <Toaster />
       </Form>
     </Formik>
   )
